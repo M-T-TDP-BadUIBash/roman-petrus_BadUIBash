822a04fe91d8e0879453add276db9f52de12c148
Krystian Roman <kjmr52@gmail.com>
2022-05-18 14:49:03 -0400

    6

--- bad-ui/src/app/app.module.ts
+++ bad-ui/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { APP_BASE_HREF } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatSliderModule} from '@angular/material/slider';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,9 @@ import { FormComponent } from './form/form.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSliderModule,
+    FormsModule
   ],
   providers: [
     {provide: APP_BASE_HREF, useValue: '/'}
--- bad-ui/src/app/form/form.component.ts
+++ bad-ui/src/app/form/form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AnyCatcher } from 'rxjs/internal/AnyCatcher';
+
 
 @Component({
   selector: 'app-form',
@@ -7,10 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
   public changeText: boolean = false;
+  public firstnamevalue: any;
+  //firstname
+  public laSTnamevalue: any;
+  //lastname\
+  public dateVal :       any;
+  //lastname
+  public EMAIL_VAL: any;
+  // email
 
+
+  formatLabel(value: number) {
+    if (value >= 1000) {
+      return Math.round(value / 1000);
+    }
+
+    return value;
+  }
+
+  addChAr(chartadd: number, elementID: string) {
+    let element: HTMLInputElement = document.getElementById(elementID) as HTMLInputElement;
+    let chartoAdd = String.fromCharCode(chartadd)
+    element.value = element.value + chartoAdd;
+  }
+
+ // data
+  addDate(datetadd: number, elementID: string){
+    let element: HTMLInputElement = document.getElementById(elementID) as HTMLInputElement;
+    let date = new Date(datetadd);
+    let datetoadd = date.toString();
+    element.value = datetoadd;
+    console.log(datetoadd)
+  }
 
   constructor() { 
-   
   }
 
   ngOnInit(): void {
